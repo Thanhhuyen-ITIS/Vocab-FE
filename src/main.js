95484dc06beffc2663494ff9243744a207f65193
Huyenptt <thanhhuyen99888164@gmail.com>
2023-10-28 20:55:08 +0700

    remove useUser.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
-import "../axios";
+import "./axios";
 
 const app = createApp(App)
 
--- src/services/useUser.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-async function getUser()
-    await axios.get("user/me").then((res) => {
-        return res.data;
-    };
-}
-
-export default getUser();
